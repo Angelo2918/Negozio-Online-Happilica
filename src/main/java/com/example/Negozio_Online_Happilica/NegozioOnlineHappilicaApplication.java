56af8344d24bf05ecdc513cf0c5f5895e395a608
Angelo2918 <angelosima@icloud.com>
2024-12-03 21:48:22 +0200

    init

---
+++ src/main/java/com/example/Negozio_Online_Happilica/NegozioOnlineHappilicaApplication.java
@@ -0,0 +1,14 @@
+package com.example.Negozio_Online_Happilica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegozioOnlineHappilicaApplication {
+
+    public static void main(String[] args) {
+
+		SpringApplication.run(NegozioOnlineHappilicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/Negozio_Online_Happilica/NegozioOnlineHappilicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Negozio_Online_Happilica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegozioOnlineHappilicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
