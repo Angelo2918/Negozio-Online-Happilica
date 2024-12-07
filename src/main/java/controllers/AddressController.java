3a734cdcc72b88c304dad6e7dc3d50f01c19022a
Angelo2918 <angelosima@icloud.com>
2024-12-07 23:53:56 +0200

    Today07-12

--- src/main/java/controllers/AddressController.java
+++ src/main/java/controllers/AddressController.java
@@ -3,12 +3,12 @@ package controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import services.UserService;
 
 @RestController
-@RequestMapping("api/users")
-public class UserController {
-@Autowired
-    private UserService userService;
+@RequestMapping(name = "/address")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
 
 }
--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -5,31 +5,36 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 /**
- * Represents an address entity with detailed location information such as country, city, street, and house number.
+ * Represents an address entity with details such as street number, city, country, and pincode.
+ * This class is annotated with @Entity to mark it as a JPA entity.
+ * It uses Lombok annotations such as @Data, @NoArgsConstructor, @AllArgsConstructor, and @ToString for code generation.
  */
 @Entity
-@Table(name = "addresses")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@ToString
 public class Address {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private Long id;
-    private String country;
+    private Integer addressId;
+    @NotNull(message = "Please fill street number!")
+    private String streetNo;
+    @NotNull(message = "Please fill City name!")
     private String city;
-    private String street;
-    private String numberOfStreet;
-
-    @OneToOne(mappedBy = "address")
-    @JsonBackReference
-    private User user;
+    @NotNull(message = "Please fill country name!")
+    private String country;
+    @NotNull(message = "Please input a valid postal code!")
+    private String pincode;
 
 }
